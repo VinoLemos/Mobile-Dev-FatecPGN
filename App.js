5c5f2baa499e4aed5a66b51fa223580e24212e25
Aluno <aluno@ceetepspg.local>
2022-09-10 10:30:33 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button, Pressable} from 'react-native';
+ 
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  
+  increase(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  decrease(){
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+ 
+  render(){
+    return(
+      <View style={styles.container}>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Text style={styles.title}>Contador de Pessoas</Text>
+      
+      <Text style={styles.counter}>{this.state.count}</Text>
+
+      <Pressable style={styles.increaseButton} onPress={() => this.increase()}>
+        <Text style={styles.textButton}>+</Text>
+      </Pressable>
+
+      <Pressable style={styles.decreaseButton} onPress={() => this.decrease()}>
+        <Text style={styles.textButton}>-</Text>
+      </Pressable>
+ 
+      </View>
+    );
+  }
 }
-
+ 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 80,
   },
-});
+  title:{
+    fontSize: 50,
+    textAlign: 'center',
+    color: 'gray',
+    fontFamily: 'Cambria',
+  },
+  counter:{
+    alignSelf: 'center',
+    fontSize: 80,
+    margin: 50
+  },
+  increaseButton:{
+    width: 200,
+    height: 50,
+    margin: 50,
+    backgroundColor: 'green'
+  },
+  decreaseButton:{
+    width: 200,
+    height: 50,
+    margin: 50,
+    backgroundColor: 'red'
+  },
+  textButton:{
+    alignSelf: 'center',
+    paddingTop: 5,
+    fontSize: 24,
+  }
+})
+ 
+export default App;
