5422d108812937f1e484aa56ea9d6d192a0b8ecd
Talita-S <talitassantos2015@gmail.com>
2022-09-29 10:25:07 -0300

    App Vagas criado

---
+++ Vagas/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, ScrollView } from 'react-native';
+import { styles } from './styles';
+
+class App extends Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <ScrollView>
+          <View>
+            
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+export default App;
