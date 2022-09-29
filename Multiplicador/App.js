2e694c742790a31fbe54e6bb5e8efa4c7e27083a
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-09-29 13:42:03 -0300

    Multiplicador

--- Multiplicador/App.js
+++ Multiplicador/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-class App extends Component {
-	render(){
-		return(
-		)
-	}
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,5 +18,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-export default App;
