09ab462a1d5b0b8e7c4c953b5d9ddc6bbbb96f6e
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-09-30 16:50:28 -0300

    Scrollview horizontal estatico

--- Anuncios/App.js
+++ Anuncios/App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, ScrollView, Dimensions, Image, ToastAndroid } from 'react-native'
+import styles from './styles'
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window')
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const cardPerSlide = 2
+const cardPadding = 15
+const paddingAround = cardPadding * 2 // slide horizontal padding
+const cardBetweenPadding = cardPadding * (cardPerSlide - 1)
+const totalPadding = paddingAround + cardBetweenPadding
+const imageWidth = (screenWidth - totalPadding) / cardPerSlide
+const imageHeight = (imageWidth / (2 / 3))
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.navBar}>
+          <Text style={styles.navBarTitle}>MOVIES</Text>
+        </View>
+        <ScrollView
+          ref={(ref) => { this.stepCarousel = ref }}
+          contentContainerStyle={styles.scrollViewContainerStyle}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: screenWidth, paddingHorizontal: cardPadding }}>
+            <View style={{ width: imageWidth }}>
+              <Image source={{ uri: 'https://image.tmdb.org/t/p/w1280/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg' }}
+                style={{ width: imageWidth, height: imageHeight }} />
+            </View>
+            <View style={{ width: imageWidth }}>
+              <Image source={{ uri: 'https://image.tmdb.org/t/p/w1280/z7FCF54Jvzv9Anxyf82QeqFXXOO.jpg' }}
+                style={{ width: imageWidth, height: imageHeight }} />
+            </View>
+            <View style={{ width: imageWidth }}>
+              <Image source={{ uri: 'https://image.tmdb.org/t/p/w1280/tgcrYiyG75iDcyk3en9NzZis0dh.jpg' }} style={{ width: imageWidth, height: imageHeight }} />
+            </View>
+          </View>
+          
+        </ScrollView>
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
