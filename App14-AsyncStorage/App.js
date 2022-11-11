1be8b2be31edc6cca5397fbbb0078fe6e3f90a45
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-11-11 18:45:20 -0300

    Adicionada Random Quote API

--- App14-AsyncStorage/App.js
+++ App14-AsyncStorage/App.js
@@ -1,32 +1,90 @@
-import React, { Component } from 'react'
-import {View, Text, StyleSheet, TextInput, TouchableOpacity, Keyboard, Switch} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {View, Text, TouchableOpacity, } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { styles } from './styles';
- 
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { styles } from './styles'
+import { StatusBar } from 'expo-status-bar';
+import Tts from 'react-native-tts';
+
+
+Tts.setDefaultLanguage('en-US');
+Tts.setDefaultVoice('com.apple.ttsbundle.Moira-compact');
+Tts.setDefaultRate(0.5);
+Tts.setDefaultPitch(1.2);
+
 export default function App(){
+  
+  const [Quote, setQuote] = useState('Loading...');
+  const [Author, setAuthor] = useState('Loading...');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const randomQuote = () => {
+    setIsLoading(true);
+    fetch("https://api.quotable.io/random").then(response => response.json()).then(result => {
+      //console.log(result.content);
+      setQuote(result.content);
+      setAuthor(result.author);
+      setIsLoading(false);
+    })
+  }
+
+  const speakNow = () => {
+    Tts.speak(Quote);
+  }
+
+  useEffect(() => {
+    randomQuote();
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Frases</Text>
 
-    <View style={styles.botoes}>
-    <Text style={{paddingRight: '10px', paddingLeft:'10px', fontSize: 20}}>
-      Dark Mode</Text>
-      <Switch 
-      />
+      <StatusBar barStyle="light-content"/>
 
-    <Text>                                  </Text>
+        <View style={styles.quoteContainer}>
 
-    <Text style={{paddingRight: '10px', paddingLeft:'10px', fontSize: 20}}>
-      Dark Mode</Text>
-      <Switch 
-      />    
-    
-    </View>
+          <Text style={styles.text}>Quote of the day</Text>
 
-    <View style={styles.frases}>
-      <Text>Test</Text>
-    </View>
+          <FontAwesome5 name="quote-left" style={{fontSize:20, marginBottom: -12}} color="#000"/>    
 
+          <Text style={styles.quote}>
+            {Quote}
+          </Text>
+
+          <FontAwesome5 name="quote-right" style={{fontSize:20, textAlign:'right', marginTop: -50, marginBottom: 20}} color="#000"/>        
+
+          <Text style={styles.author}>— {Author} </Text>
+
+          <TouchableOpacity onPress={randomQuote} 
+          style={{backgroundColor: isLoading ? 'rgba(83, 114, 240, 0.7)' : 'rgba(83, 114, 240, 1)',
+                  padding: 20,
+                  borderRadius: 30,
+                  marginVertical: 20,}}>
+            
+            <Text style={styles.newQuote}>
+              {isLoading ? "Loading..." : "New Quote"}
+            </Text>
+
+          </TouchableOpacity>
+
+          <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+
+            <TouchableOpacity onPress={speakNow} style={styles.buttons}>
+                <FontAwesome name="volume-up" size={22} color='#53272F0'/>  
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => {}} style={styles.buttons}>
+                <FontAwesome5 name="clipboard" size={22} color='#53272F0'/>  
+            </TouchableOpacity>    
+
+            <TouchableOpacity onPress={() => {}} style={styles.buttons}>
+                <FontAwesome name="twitter" size={22} color='#53272F0'/>  
+            </TouchableOpacity>    
+
+          </View>
+
+        </View>
     </View>
   )
 }
--- App14-AsyncStorage/styles.js
+++ App14-AsyncStorage/styles.js
@@ -2,27 +2,51 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container:{
-      flex: 1,
-      marginTop: 20,
-      alignItems: 'center'
-    },
-    botoes: {
-        flexDirection: 'row'
-    },
-    title: {
+        flex: 1,
         justifyContent: 'center',
-        padding: '30px',
-        fontSize: '30px',
-        borderBottomColor: 'black',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-    },
-    frases: {
         alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 30,
-        width: '80%',
-        height: '50%',
-        borderWidth: '5px'
+        backgroundColor: '#5372f0'
+    },
+    quoteContainer:{
+        width: '90%',
+        backgroundColor: '#fff',
+        borderRadius: '20px',
+        padding: 20
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 26,
+        fontWeight: 600,
+        color: '#333',
+        marginBottom: 20,
+    },
+    quote:{
+        color: '#000',
+        fontSize: 16,
+        lineHeight: 26,
+        letterSpacing: 1.1,
+        fontWeight: 400,
+        textAlign: 'center',
+        marginBottom: 10,
+        paddingHorizontal: 30
+    },
+    newQuote:{
+        color:'#fff',
+        fontSize: 18,
+        textAlign: 'center',
+    },
+    author:{
+        textAlign:'right',
+        fontWeight: 300,
+        fontStyle:'italic',
+        fontSize: 16,
+        fontColor: '#000'
+    },
+    buttons:{
+        borderWidth: 2,
+        borderColor: '#5372F0',
+        borderRadius: 50,
+        padding: 15,
     }
   });
 
