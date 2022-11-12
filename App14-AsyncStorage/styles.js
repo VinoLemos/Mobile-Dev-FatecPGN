c58eb7989bee29825aaad815c6ee3f5d846ae9b7
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2022-11-11 22:16:14 -0300

    Adicionado TTS, Copy to clipboard & TweetNow

--- App14-AsyncStorage/styles.js
+++ App14-AsyncStorage/styles.js
@@ -1,13 +1,14 @@
 import { StyleSheet } from 'react-native';
+import { lightMode } from './App';
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#5372f0'
+        backgroundColor: lightMode ? 'black' : '#5372f0'
     },
-    quoteContainer:{
+    quoteContainer: {
         width: '90%',
         backgroundColor: '#fff',
         borderRadius: '20px',
@@ -16,38 +17,43 @@ const styles = StyleSheet.create({
     text: {
         textAlign: 'center',
         fontSize: 26,
-        fontWeight: 600,
+        fontWeight: '600px',
         color: '#333',
         marginBottom: 20,
     },
-    quote:{
+    quote: {
         color: '#000',
         fontSize: 16,
         lineHeight: 26,
         letterSpacing: 1.1,
-        fontWeight: 400,
+        fontWeight: '400px',
         textAlign: 'center',
         marginBottom: 10,
         paddingHorizontal: 30
     },
-    newQuote:{
-        color:'#fff',
+    newQuote: {
+        color: '#fff',
         fontSize: 18,
         textAlign: 'center',
     },
-    author:{
-        textAlign:'right',
-        fontWeight: 300,
-        fontStyle:'italic',
+    author: {
+        textAlign: 'right',
+        fontWeight: '300px',
+        fontStyle: 'italic',
         fontSize: 16,
         fontColor: '#000'
     },
-    buttons:{
+    buttons: {
         borderWidth: 2,
         borderColor: '#5372F0',
         borderRadius: 50,
         padding: 15,
+    },
+    switch: {
+        alignSelf:'left',
+        justifyContent: 'left',
+        alignItems: 'left'
     }
-  });
+});
 
-  export { styles };
+export { styles };
