2d54f31ef8d4a8c312a7dde4ca955c8271e03ce0
Talita Santos <talita-s@ibm.com>
2022-12-01 15:48:07 -0300

    Estrutura criada

--- App17-ViaCep/App.js
+++ App17-ViaCep/App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>CEP X Endereço </Text>
+
+      <TextInput
+        placeholder='Digite o CEP...'
+      />
+
+      <TouchableOpacity>
+        <AntDesign name='checksquareo'/>
+      </TouchableOpacity>
+
     </View>
   );
 }
