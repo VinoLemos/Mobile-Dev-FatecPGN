dc10a78d80a77a7c3e1800c6d675bca1c20950ad
Talita Santos <talita-s@ibm.com>
2022-12-01 23:14:55 -0300

    Projeto estruturado

--- App18-GitHubApi/App.js
+++ App18-GitHubApi/App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Perfil dos Devs</Text>
+
+      <TextInput
+        placeholder='Digite o login Git'
+      />
+
+      <TouchableOpacity>
+        <AntDesign name='checksquareo'/>
+      </TouchableOpacity>
+
     </View>
   );
 }
