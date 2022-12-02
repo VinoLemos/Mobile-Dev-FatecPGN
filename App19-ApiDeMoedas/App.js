8c81ec981d54d15066e218ae21eb85a5a36f6914
Talita Santos <talita-s@ibm.com>
2022-12-01 23:58:21 -0300

    Projeto estruturado

--- App19-ApiDeMoedas/App.js
+++ App19-ApiDeMoedas/App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Picker } from 'react-native';
+
+import api from './src/services/api';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Conversor de moedas</Text>
+
+      <Text>Valor: <TextInput/> </Text>
+
+      <Text>De: 
+        <Picker>
+          <Picker.Item value="" label="Selecionar"/>
+          <Picker.Item value="" label="Real"/>
+          <Picker.Item value="" label="Dólar"/>
+          <Picker.Item value="" label="Euro"/>
+          <Picker.Item value="" label="Bitcoin"/>
+        </Picker>
+      </Text>
+      <Text>Para: 
+      <Picker>
+          <Picker.Item value="" label="Selecionar"/>
+          <Picker.Item value="" label="Real"/>
+          <Picker.Item value="" label="Dólar"/>
+          <Picker.Item value="" label="Euro"/>
+          <Picker.Item value="" label="Bitcoin"/>
+        </Picker>
+      </Text>
+
     </View>
   );
 }
