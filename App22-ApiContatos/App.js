82517367f1906b9f0efa82950c7badf428baaa19
Talita-S <talitassantos2015@gmail.com>
2022-12-02 23:47:56 -0300

    Funcao app ajustada

--- App22-ApiContatos/App.js
+++ App22-ApiContatos/App.js
@@ -1,9 +1,18 @@
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Contatos from './src/Contatos/index';
+import Form from './src/Form/index';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Contatos' component={Contatos}/>
+        <Stack.Screen name='Formulario' component={Form}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
